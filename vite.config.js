52c1fcd4defe8a548ee3b21ae757c687348ac1de
renaudlesage-hub <renaud.lesage@bucolique.be>
2026-07-13 22:25:15 +0200

    nn

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // vite.config.js
 export default defineConfig({
-  base: './', // Utilise des chemins relatifs pour éviter les erreurs 404
+  base: './', // <-- LIGNE CRUCIALE pour éviter les erreurs 404 du CSS
   plugins: [react()],
-});
+})
