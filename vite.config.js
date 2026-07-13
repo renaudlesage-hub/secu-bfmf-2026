7e993af01d3e41fe9c9d6d3398479d92644ea482
renaudlesage-hub <renaud.lesage@bucolique.be>
2026-07-13 22:07:55 +0200

    nn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+// vite.config.js
+export default defineConfig({
+  base: './', // Utilise des chemins relatifs pour éviter les erreurs 404
+  plugins: [react()],
+});
 export default defineConfig({
   plugins: [
     react(),
